87d8c4c7dd71bbf52dc1d9127070f001d9e63bbb
Omar Blas <blas0123.22@gmail.com>
2024-05-31 18:18:50 -0500

    add proyecto signalR

---
+++ SignalReactive/Controllers/BeerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalReactive.Hubs;
+
+namespace SignalReactive.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private IHubContext<MessageHub> _hubContext;
+
+        public BeerController(IHubContext<MessageHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Send(string message)
+        {
+            await _hubContext.Clients.All.SendAsync(message);
+            return Ok();
+        }
+    }
+}
---
+++ SignalReactive/Program.cs
@@ -0,0 +1,36 @@
+using SignalReactive.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    _ = endpoints.MapHub<MessageHub>("/message");
+});
+
+app.Run();
